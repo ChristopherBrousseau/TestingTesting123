d80e3d7248579406db08e2601368f58b2e9e4127
ChristopherBrousseau <156946433+ChristopherBrousseau@users.noreply.github.com>
2024-12-04 15:10:21 -0500

    Completed assignment

--- app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
+++ app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
@@ -1,5 +1,6 @@
 package com.example.testingtesting123
 
+import junit.framework.TestCase.assertEquals
 import org.junit.Before
 import org.junit.Test
 
@@ -16,14 +17,25 @@ class UniqueCollectionTest {
     // TODO 1: Write a test to ensure items can be added to the collection
     @Test
     fun addAnItem() {
+        val item = Item("TestItem")
+        collection.addItem(item)
 
+        assertEquals(1, collection.size())
+        assertEquals(item, collection.get(0))
     }
 
     // TODO 2: Write a test to ensure that only unique items can be added to the collection
     // Uniqueness is determined by the Item.name property, which is set via the constructor
     @Test
     fun addUniqueItem() {
+        val item1 = Item("TestItem")
+        val item2 = Item("TESTITEM")
 
+        collection.addItem(item1)
+        collection.addItem(item2)
+
+        assertEquals(1, collection.size())
+        assertEquals(item1, collection.get(0))
     }
 
     // Test Driven Development (TDD) test - complete specified function so that this test passes
